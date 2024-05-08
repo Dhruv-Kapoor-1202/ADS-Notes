036fa3c997984c26796acafb596c30cc06d3d783
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-05-08 12:08:17 +0530

    diagram

--- May/Tries/Test.java
+++ May/Tries/Test.java
@@ -2,22 +2,11 @@ package May.Tries;
 
 public class Test {
 
-  static int start = 1;
-
-  final int end;
-
-  public Test(int x) {
-    x = 2;
-    end = x;
-  }
-
-  public void fly(int distance) {
-    System.out.println(end - start);
-    System.out.println(distance);
-  }
-
   public static void main(String[] args) {
-    new Test(15).fly(5);
-    // new Test(20).fly(10);
+    int x = -148;
+    x = x >>> 31;
+    System.out.println(x);
+    // 128 64 32 16 8 4 2 1
+
   }
 }
